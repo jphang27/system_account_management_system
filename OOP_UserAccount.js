81887834846ae7cfdc77213ed8930de8ddf65a71
John Phang <jphang@talentpath.com>
2019-10-16 15:25:29 -0500

    attempting to right OOP signUp()

--- OOP_UserAccount.js
+++ OOP_UserAccount.js
@@ -1,5 +1,28 @@
-user = {};
-function signUp(user){};
-function singIn(user){};
-function singOut(user){};
-function passwordChange(user);
+class User{
+    constructor(email, password, passwordConfirmation){
+        this.userEmail = email;
+        this.userPassword = password;
+        this.userPasswordConfirmation = passwordConfirmation;
+    };
+
+    // signUp(user){
+
+    // };
+    signUp(user){
+        console.log("user1 info is: " + user1.userEmail + " " + "password: " + user1.userPassword + " " + "passwordconfirmation: " + user1.userPasswordConfirmation);
+    };
+    singIn(user){
+    
+    };
+    singOut(user){
+    
+    };
+    passwordChange(user){
+    
+    };
+    
+}
+
+let user1 = new User("jphang", "xyz", "xyz");
+//console.log("user1 info is: " + user1.userEmail + " " + "password: " + user1.userPassword + " " + "passwordconfirmation: " + user1.userPasswordConfirmation);
+user1.signUp();
